e78da2dc37f711684f19fff4d1bd73720e207220
Shwetank Thakur <129889206+Shwetank0620@users.noreply.github.com>
2025-05-07 00:02:27 +0530

    Time: 0 ms (100%), Space: 42.1 MB (7.99%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorderHelper(root, result);
+        return result;
+    }
+
+    private void preorderHelper(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+        result.add(node.val);                
+        preorderHelper(node.left, result);   
+        preorderHelper(node.right, result);  
+    }
+}
