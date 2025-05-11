89d376376a5b6fdca6046bc06fb72d80a3337e4a
Shwetank Thakur <129889206+Shwetank0620@users.noreply.github.com>
2025-05-12 02:17:50 +0530

    Time: 1 ms (73.16%), Space: 45.6 MB (82.75%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+       int m = matrix.length, n = matrix[0].length;
+        boolean firstRowZero = false, firstColZero = false;
+        
+        // Determine if the first row or first column should be zero
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+        
+        // Use first row and column to mark zero rows and columns
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        // Set matrix elements to zero based on marks
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        // Set the first row to zero if needed
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        
+        // Set the first column to zero if needed
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+} 
+    
