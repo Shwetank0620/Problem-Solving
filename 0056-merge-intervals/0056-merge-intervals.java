af485805249c5ac3c73f59ee7f527993e5f33734
Shwetank Thakur <129889206+Shwetank0620@users.noreply.github.com>
2025-07-19 00:31:17 +0530

    Time: 8 ms (85.97%), Space: 46.8 MB (25.74%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,28 +1,22 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1) return intervals;
-        
-        // Sort intervals by starting time
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        
+
         List<int[]> merged = new ArrayList<>();
-        int[] currentInterval = intervals[0];
-        merged.add(currentInterval);
-        
-        for (int[] interval : intervals) {
-            int currentEnd = currentInterval[1];
-            int nextStart = interval[0];
-            int nextEnd = interval[1];
-            
-            if (nextStart <= currentEnd) { // Overlapping intervals
-                currentInterval[1] = Math.max(currentEnd, nextEnd);
-            } else { // Non-overlapping interval
-                currentInterval = interval;
-                merged.add(currentInterval);
+        int[] prev = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+            if (interval[0] <= prev[1]) {
+                prev[1] = Math.max(prev[1], interval[1]);
+            } else {
+                merged.add(prev);
+                prev = interval;
             }
         }
-        
-        return merged.toArray(new int[merged.size()][]);
+
+        merged.add(prev);
+
+        return merged.toArray(new int[merged.size()][]);         
     }
 }
-    
