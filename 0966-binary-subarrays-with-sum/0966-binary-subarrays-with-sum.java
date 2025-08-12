75e18a9bf712b625d9de863a2fb4d062cd20315e
Shwetank Thakur <129889206+Shwetank0620@users.noreply.github.com>
2025-08-12 12:05:26 +0530

    Time: 3 ms (51.58%), Space: 47.5 MB (85.1%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,22 +1,42 @@
-import java.util.HashMap;
-
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        HashMap<Integer, Integer> prefixSumCount = new HashMap<>();
-        prefixSumCount.put(0, 1); 
-        
-        int sum = 0, result = 0;
-
-        for (int num : nums) {
-            sum += num;
-            
-            if (prefixSumCount.containsKey(sum - goal)) {
-                result += prefixSumCount.get(sum - goal);
-            }
-
-            prefixSumCount.put(sum, prefixSumCount.getOrDefault(sum, 0) + 1);
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+    
+    public int atMost(int[] nums, int goal) {
+        if (goal < 0) {
+            return 0;
         }
 
-        return result;
+        int l = 0, sum = 0, count = 0;
+        for (int r = 0; r < nums.length; r++) {
+            sum += nums[r];
+            while (sum > goal) {
+                sum -= nums[l];
+                l++;
+            }
+            count += (r - l + 1);
+        }
+        return count;
     }
 }
+
+/*class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        
+        if(goal<0){
+            return 0;
+        }
+        int l=0, r=0, sum=0, cnt=0;
+        while(r<=nums.length){
+            sum += nums[r];
+            while(sum>goal){
+                sum=sum-nums[l];
+                l=l+1;
+            }
+            cnt=cnt+(r-l+1);
+            r=r+1;
+        }
+        return cnt;
+    }
+}*/
